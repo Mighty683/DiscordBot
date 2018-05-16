3f8aa82a53763a4e48f0162ba15ce87b7e2eb9a4
Tomasz Szarek <tomasz.szarek@sollers.eu>
2018-05-16 11:32:20 +0200

    Mongo DB wprowadzenie part 2 - wyciąganie danych z bazy.

--- index.js
+++ index.js
@@ -13,56 +13,56 @@ function initDB () {
 
 function WakaiListenerInit (channel) {
   channel.on('message:received', (msg) => {
-      if (msg.mentions.users.find(channel.isUserBot.bind(channel))) {
-          if (msg.content.includes('slap')) {
-              channel.sendMessage({channelId: msg.channel.id, string: 'Fuminator slaps ' + msg.author + '!'})
-          }
+    if (msg.mentions.users.find(channel.isUserBot.bind(channel))) {
+      if (msg.content.includes('slap')) {
+        channel.sendMessage({channelId: msg.channel.id, string: 'Fuminator slaps ' + msg.author + '!'})
       }
+    }
   })
 
   channel.on('fumi:cmd:received', () => {
-      channel.sendMessage({
-          title: 'Fumi na dziś',
-          file: {
-              attachment: '/home/migum/fuminatorData/' + Math.floor((Math.random() * 6) + 1) + '.jpg',
-              name: 'fumi.jpg'
-          }
-      })
+    channel.sendMessage({
+      title: 'Fumi na dziś',
+      file: {
+        attachment: '/home/migum/fuminatorData/' + Math.floor((Math.random() * 6) + 1) + '.jpg',
+        name: 'fumi.jpg'
+      }
+    })
   })
 
   channel.on('help:cmd:received', (msg) => {
-      channel.sendMessage({
-          desc: 'f!fumi - random fumi quote\nf!hug - hugs!\nf!who - current radio dj\nf!song - current song title\nf!listeners - current number of listeners'
-      })
+    channel.sendMessage({
+      desc: 'f!fumi - random fumi quote\nf!hug - hugs!\nf!who - current radio dj\nf!song - current song title\nf!listeners - current number of listeners'
+    })
   })
 }
 
 function A24ListenerInit (channel) {
   channel.on('help:cmd:received', (msg) => {
-      channel.sendMessage({
-          desc: 'f!hug - hugs!\nf!who - current radio dj\nf!song - current song title\nf!listeners - current number of listeners',
-          channelId: msg.channel.id
-      })
+    channel.sendMessage({
+      desc: 'f!hug - hugs!\nf!who - current radio dj\nf!song - current song title\nf!listeners - current number of listeners',
+      channelId: msg.channel.id
+    })
   })
 }
 
-function channelInit (apiChannel) {
-    var channel = new Channel({
-      config: config,
-      apiChannel: apiChannel
-    })
+function channelInit (apiChannel, dbDocConnection) {
+  var channel = new Channel({
+    apiChannel: apiChannel,
+    dbDocConnection: dbDocConnection
+  })
 
-    channel.on('hug:cmd:received', (msg) => {
-      let userToHug = msg.mentions.users.first() || msg.author
-      channel.sendMessage({channelId: msg.channel.id, string: 'Fuminator hug ' + userToHug + '!'})
-    })
+  channel.on('hug:cmd:received', (msg) => {
+    let userToHug = msg.mentions.users.first() || msg.author
+    channel.sendMessage({channelId: msg.channel.id, string: 'Fuminator hug ' + userToHug + '!'})
+  })
 
-    if (channel.name === 'Wakai') {
-      WakaiListenerInit(channel)
-    } else {
-      A24ListenerInit(channel)
-    }
-    channel.start()
+  if (channel.name === 'Wakai') {
+    WakaiListenerInit(channel)
+  } else {
+    A24ListenerInit(channel)
+  }
+  channel.start()
 }
 
 function start () {
@@ -70,20 +70,24 @@ function start () {
   dbController.on('collection:set', (collection) => {
     config.channels.forEach((channel) => {
       collection.find(channel).next((err, doc) => {
-        if (doc) {
-          console.log(`DB:${channel.name}:doc:exists`)
-          channelInit(channel)
+        if (!err) {
+          if (doc) {
+            console.log(`DB:${channel.name}:doc:exists`)
+            channelInit(doc, collection.find(channel))
+          } else {
+            console.log(`DB:${channel.name}:doc:created`)
+            collection.insertOne(channel, (err, doc) => {
+              if (!err) {
+                channelInit(doc.ops[0], collection.find(channel))
+              }
+            })
+          }
         } else {
-          console.log(`DB:${channel.name}:doc:created`)
-          collection.insertOne(channel, (err, doc) => {
-            if(!err) {
-              channelInit(channel)
-            }
-          })
+          console.log('Doc access error!')
         }
       })
     })
   })
 }
 
-start ()
+start()
